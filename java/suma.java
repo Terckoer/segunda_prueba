17ce691dabba0c40dd2e90781a3622b78dc253a5
terckoer <prueba@gmail.com>
2020-08-31 22:50:40 -0500

    Agrego metodo restar

--- java/suma.java
+++ java/suma.java
@@ -4,10 +4,16 @@ public class suma{
 public static void main (String [] args){
 
 System.out.println(sumar(10,20));
+System.out.println(restar(20,10););
 }
 
 
     public static int sumar(int n1, int n2){
         return n1+n2;
+    }
+
+    public static int restar (int n1, int n2){
+        return n1-n2;
+
     }
 }
