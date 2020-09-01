580fd68933241248190fac615fa37a5200f52ab0
terckoer <prueba@gmail.com>
2020-08-31 22:37:07 -0500

    Aprendiendo otra vez

---
+++ java/suma.java
@@ -0,0 +1,13 @@
+
+public class suma{
+
+public static void main (String [] args){
+
+System.out.println(sumar(10,20));
+}
+
+
+    public static int sumar(int n1, int n2){
+        return n1+n2;
+    }
+}
